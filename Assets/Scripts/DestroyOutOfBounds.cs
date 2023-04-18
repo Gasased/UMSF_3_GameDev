449fa349209d91c0e84ed0d0d5166c3d07744c32
Gasased <46537138+Gasased@users.noreply.github.com>
2023-04-18 11:49:55 +0300

    Minor changes

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -14,15 +14,6 @@ public class DestroyOutOfBounds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
-            Destroy(gameObject);
-        }
-
-        if (transform.position.z > topBound)
-        {
-            Destroy(gameObject);
-        }
-        else if (transform.position.z < lowerBound)
-        {
             Debug.Log("Game Over!");
             Destroy(gameObject);
         }
